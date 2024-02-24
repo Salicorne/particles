90e80ebe6a20f0bbff7e9425b150cb0131fc7496
Salicorne <timothee.raboeuf@gmail.com>
2024-02-24 12:36:46 +0100

    Refactor settings to distinguish population ids from their color

--- main.go
+++ main.go
@@ -29,26 +29,34 @@ type entity struct {
 }
 
 type Rule struct {
-	Color1      string  `json:"c1,omitempty"`
-	Color2      string  `json:"c2,omitempty"`
-	Force       float64 `json:"f,omitempty"`
-	EffectRange float64 `json:"r,omitempty"`
+	Pop1        PopulationId `json:"c1,omitempty"`
+	Pop2        PopulationId `json:"c2,omitempty"`
+	Force       float64      `json:"f,omitempty"`
+	EffectRange float64      `json:"r,omitempty"`
 }
 
+type Population struct {
+	Count uint   `json:"n"`
+	Color string `json:"c"`
+}
+
+type PopulationId string
+
 type App struct {
-	ctx      js.Value
-	window   js.Value
-	size     vec2i
-	entities map[string][]*entity
-	rules    []Rule
+	ctx         js.Value
+	window      js.Value
+	size        vec2i
+	entities    map[PopulationId][]*entity
+	populations map[PopulationId]Population
+	rules       []Rule
 
 	globalLoss float64
 	mutex      sync.Mutex
 }
 
 type Settings struct {
-	Colors map[string]uint `json:"c,omitempty"`
-	Rules  []Rule          `json:"r,omitempty"`
+	Populations map[PopulationId]Population `json:"p,omitempty"`
+	Rules       []Rule                      `json:"r,omitempty"`
 }
 
 func newApp() (*App, error) {
@@ -83,12 +91,13 @@ func newApp() (*App, error) {
 	canvas.Set("height", rect.Get("height").Int())
 
 	return &App{
-		ctx:        ctx,
-		window:     jsWindow,
-		size:       vec2i{x: rect.Get("width").Int(), y: rect.Get("height").Int()},
-		entities:   map[string][]*entity{},
-		rules:      []Rule{},
-		globalLoss: 0.98,
+		ctx:         ctx,
+		window:      jsWindow,
+		size:        vec2i{x: rect.Get("width").Int(), y: rect.Get("height").Int()},
+		entities:    map[PopulationId][]*entity{},
+		populations: map[PopulationId]Population{},
+		rules:       []Rule{},
+		globalLoss:  0.98,
 	}, nil
 }
 
@@ -97,38 +106,44 @@ func drawRect(ctx js.Value, color string, pos, size vec2i) {
 	ctx.Call("fillRect", pos.x, pos.y, size.x, size.y)
 }
 
-func (app *App) addEntity(color string) {
-	if _, ok := app.entities[color]; !ok {
-		app.entities[color] = []*entity{}
+func (app *App) addEntity(population PopulationId) {
+	if _, ok := app.entities[population]; !ok {
+		app.entities[population] = []*entity{}
 	}
 
-	initialPos := vec2f{float64(rand.Intn(app.size.x)), float64(rand.Intn(app.size.y))}
-	app.entities[color] = append(app.entities[color], &entity{
-		pos:    initialPos,
-		newPos: initialPos,
-		speed:  vec2f{},
-	})
+	if _, ok := app.populations[population]; ok {
+		initialPos := vec2f{float64(rand.Intn(app.size.x)), float64(rand.Intn(app.size.y))}
+		app.entities[population] = append(app.entities[population], &entity{
+			pos:    initialPos,
+			newPos: initialPos,
+			speed:  vec2f{},
+		})
+	}
 }
 
-func (app *App) addRule(color1, color2 string, force float64, effectRange float64) {
-	app.rules = append(app.rules, Rule{Color1: color1, Color2: color2, Force: force, EffectRange: effectRange})
+func (app *App) addRule(pop1, pop2 PopulationId, force float64, effectRange float64) {
+	app.rules = append(app.rules, Rule{Pop1: pop1, Pop2: pop2, Force: force, EffectRange: effectRange})
 }
 
 func (app *App) initialize(settings *Settings) {
 	app.mutex.Lock()
 
 	// Reset settings
-	app.entities = map[string][]*entity{}
+	app.entities = map[PopulationId][]*entity{}
 	app.rules = []Rule{}
 
-	for c := range settings.Colors {
-		for i := uint(0); i < settings.Colors[c]; i++ {
+	for c := range settings.Populations {
+		app.populations[c] = Population{
+			Count: settings.Populations[c].Count,
+			Color: settings.Populations[c].Color,
+		}
+		for i := uint(0); i < settings.Populations[c].Count; i++ {
 			app.addEntity(c)
 		}
 	}
 
 	for r := range settings.Rules {
-		app.addRule(settings.Rules[r].Color1, settings.Rules[r].Color2, settings.Rules[r].Force, settings.Rules[r].EffectRange)
+		app.addRule(settings.Rules[r].Pop1, settings.Rules[r].Pop2, settings.Rules[r].Force, settings.Rules[r].EffectRange)
 	}
 
 	app.mutex.Unlock()
@@ -136,18 +151,21 @@ func (app *App) initialize(settings *Settings) {
 
 func (app *App) getSettings() *Settings {
 	s := &Settings{
-		Colors: map[string]uint{},
-		Rules:  []Rule{},
+		Populations: map[PopulationId]Population{},
+		Rules:       []Rule{},
 	}
 
-	for i := range app.entities {
-		s.Colors[i] = uint(len(app.entities[i]))
+	for i := range app.populations {
+		s.Populations[i] = Population{
+			Count: uint(len(app.entities[i])),
+			Color: app.populations[i].Color,
+		}
 	}
 
 	for i := range app.rules {
 		s.Rules = append(s.Rules, Rule{
-			Color1:      app.rules[i].Color1,
-			Color2:      app.rules[i].Color2,
+			Pop1:        app.rules[i].Pop1,
+			Pop2:        app.rules[i].Pop2,
 			Force:       app.rules[i].Force,
 			EffectRange: app.rules[i].EffectRange,
 		})
@@ -159,12 +177,12 @@ func (app *App) getSettings() *Settings {
 func (app *App) update() {
 	//* Apply rules
 	for u := range app.rules {
-		for i := range app.entities[app.rules[u].Color1] {
+		for i := range app.entities[app.rules[u].Pop1] {
 			force := vec2f{}
-			for j := range app.entities[app.rules[u].Color2] {
-				if app.rules[u].Color1 != app.rules[u].Color2 || i != j {
-					dx := app.entities[app.rules[u].Color1][i].pos.x - app.entities[app.rules[u].Color2][j].pos.x
-					dy := app.entities[app.rules[u].Color1][i].pos.y - app.entities[app.rules[u].Color2][j].pos.y
+			for j := range app.entities[app.rules[u].Pop2] {
+				if app.rules[u].Pop1 != app.rules[u].Pop2 || i != j {
+					dx := app.entities[app.rules[u].Pop1][i].pos.x - app.entities[app.rules[u].Pop2][j].pos.x
+					dy := app.entities[app.rules[u].Pop1][i].pos.y - app.entities[app.rules[u].Pop2][j].pos.y
 					dist := math.Sqrt(dx*dx + dy*dy)
 
 					if dist < app.rules[u].EffectRange {
@@ -174,30 +192,30 @@ func (app *App) update() {
 				}
 			}
 
-			app.entities[app.rules[u].Color1][i].speed.x *= app.globalLoss
-			app.entities[app.rules[u].Color1][i].speed.y *= app.globalLoss
-			app.entities[app.rules[u].Color1][i].speed.x += force.x
-			app.entities[app.rules[u].Color1][i].speed.y += force.y
+			app.entities[app.rules[u].Pop1][i].speed.x *= app.globalLoss
+			app.entities[app.rules[u].Pop1][i].speed.y *= app.globalLoss
+			app.entities[app.rules[u].Pop1][i].speed.x += force.x
+			app.entities[app.rules[u].Pop1][i].speed.y += force.y
 
-			app.entities[app.rules[u].Color1][i].newPos.x += app.entities[app.rules[u].Color1][i].speed.x
-			app.entities[app.rules[u].Color1][i].newPos.y += app.entities[app.rules[u].Color1][i].speed.y
+			app.entities[app.rules[u].Pop1][i].newPos.x += app.entities[app.rules[u].Pop1][i].speed.x
+			app.entities[app.rules[u].Pop1][i].newPos.y += app.entities[app.rules[u].Pop1][i].speed.y
 
 			// clamp
-			if app.entities[app.rules[u].Color1][i].newPos.x < 0 {
-				app.entities[app.rules[u].Color1][i].newPos.x = 0
-				app.entities[app.rules[u].Color1][i].speed.x *= -1
+			if app.entities[app.rules[u].Pop1][i].newPos.x < 0 {
+				app.entities[app.rules[u].Pop1][i].newPos.x = 0
+				app.entities[app.rules[u].Pop1][i].speed.x *= -1
 			}
-			if app.entities[app.rules[u].Color1][i].newPos.x > float64(app.size.x) {
-				app.entities[app.rules[u].Color1][i].newPos.x = float64(app.size.x)
-				app.entities[app.rules[u].Color1][i].speed.x *= -1
+			if app.entities[app.rules[u].Pop1][i].newPos.x > float64(app.size.x) {
+				app.entities[app.rules[u].Pop1][i].newPos.x = float64(app.size.x)
+				app.entities[app.rules[u].Pop1][i].speed.x *= -1
 			}
-			if app.entities[app.rules[u].Color1][i].newPos.y < 0 {
-				app.entities[app.rules[u].Color1][i].newPos.y = 0
-				app.entities[app.rules[u].Color1][i].speed.y *= -1
+			if app.entities[app.rules[u].Pop1][i].newPos.y < 0 {
+				app.entities[app.rules[u].Pop1][i].newPos.y = 0
+				app.entities[app.rules[u].Pop1][i].speed.y *= -1
 			}
-			if app.entities[app.rules[u].Color1][i].newPos.y > float64(app.size.y) {
-				app.entities[app.rules[u].Color1][i].newPos.y = float64(app.size.y)
-				app.entities[app.rules[u].Color1][i].speed.y *= -1
+			if app.entities[app.rules[u].Pop1][i].newPos.y > float64(app.size.y) {
+				app.entities[app.rules[u].Pop1][i].newPos.y = float64(app.size.y)
+				app.entities[app.rules[u].Pop1][i].speed.y *= -1
 			}
 		}
 	}
@@ -213,9 +231,9 @@ func (app *App) update() {
 func (app *App) render() {
 	drawRect(app.ctx, "#ddd", vec2i{}, app.size)
 
-	for color := range app.entities {
-		for i := range app.entities[color] {
-			drawRect(app.ctx, color, app.entities[color][i].pos.tovec2i(), vec2i{6, 6})
+	for pop := range app.entities {
+		for i := range app.entities[pop] {
+			drawRect(app.ctx, app.populations[pop].Color, app.entities[pop][i].pos.tovec2i(), vec2i{6, 6})
 		}
 	}
 }
@@ -248,20 +266,26 @@ func main() {
 		}
 
 		settings := &Settings{
-			Colors: map[string]uint{},
-			Rules:  []Rule{},
+			Populations: map[PopulationId]Population{},
+			Rules:       []Rule{},
 		}
 		defaultSettings := &Settings{
-			Colors: map[string]uint{
-				"red":  20,
-				"blue": 20,
+			Populations: map[PopulationId]Population{
+				"a": {
+					Count: 20,
+					Color: "blue",
+				},
+				"b": {
+					Count: 20,
+					Color: "red",
+				},
 			},
 			Rules: []Rule{
 				//{"red", "blue", -0.2, float64(app.size.x / 5.0)},
-				{"blue", "blue", -0.1, float64(app.size.x / 5.0)},
-				{"red", "red", 0.8, float64(app.size.x / 8.0)},
-				{"red", "blue", 0.5, float64(app.size.x / 5.0)},
-				{"blue", "red", -0.8, float64(app.size.x / 8.0)},
+				{"a", "a", -0.1, float64(app.size.x / 5.0)},
+				{"b", "b", 0.8, float64(app.size.x / 8.0)},
+				{"b", "a", 0.5, float64(app.size.x / 5.0)},
+				{"a", "b", -0.8, float64(app.size.x / 8.0)},
 			},
 		}
 
@@ -287,8 +311,8 @@ func main() {
 	js.Global().Set("setSettings", js.FuncOf(func(this js.Value, args []js.Value) any {
 		if app != nil {
 			settings := &Settings{
-				Colors: map[string]uint{},
-				Rules:  []Rule{},
+				Populations: map[PopulationId]Population{},
+				Rules:       []Rule{},
 			}
 
 			if len(args) > 0 {
